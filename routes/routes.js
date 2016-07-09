7f96adaed80411dd6f3622fd693824c6d6f50122
cd83 <chrisdlee@gmail.com>
2016-07-09 13:57:52 -0500

    fixes

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,22 @@
 var appRouter = function(app){
     app.get("/status", function (req, res) {
         console.log("someone hit ye ole status");
+        rockPaperScissorsBot;
         if(req.query.text){
             if (rpsbot == 'rock' && req.query.text == 'scissors') {
-                return res.send ({"text": "YOU LOSE! Bot rolled ROCK!"})
+                return res.send({"text": "YOU LOSE! Bot rolled ROCK!"});
             } else if (rpsbot == 'paper' && req.query.text == 'rock') {
-                return res.send ({"text": "YOU LOSE! Bot rolled PAPER!"})
+                return res.send({"text": "YOU LOSE! Bot rolled PAPER!"});
             } else if (rpsbot == 'scissors' && req.query.text == 'paper') {
-                return res.send ({"text": "YOU LOSE! Bot rolled SCISSORS!"})
+                return res.send({"text": "YOU LOSE! Bot rolled SCISSORS!"});
             } else if (rpsbot == 'rock' && req.query.text == 'paper') {
-                return res.send ({"text": "YOU WIN! Bot rolled ROCK!"})
+                return res.send({"text": "YOU WIN! Bot rolled ROCK!"});
             } else if (rpsbot == 'paper' && req.query.text == 'scissors') {
-                return res.send ({"text": "YOU WIN! Bot rolled PAPER!"})
+                return res.send({"text": "YOU WIN! Bot rolled PAPER!"});
             } else if (rpsbot == 'scissors' && req.query.text == 'rock') {
-                return res.send ({"text": "YOU WIN! Bot rolled SCISSORS!"})
+                return res.send({"text": "YOU WIN! Bot rolled SCISSORS!"});
             } else if (rpsbot == req.query.text) {
-                return res.send ({"text": "TIE! You both rolled " + req.query.text + "!!!"})
+                return res.send({"text": "TIE! You both rolled " + req.query.text + "!!!"})
             }
         }
         else {
