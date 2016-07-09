39ee8d350466bb1bb1f0ba6101b8af2b69bfdfd4
cd83 <chrisdlee@gmail.com>
2016-07-09 13:52:03 -0500

    initial commit

---
+++ routes/routes.js
@@ -0,0 +1,46 @@
+var appRouter = function(app){
+    app.get("/status", function (req, res) {
+        console.log("someone hit ye ole status");
+        if(req.query.text){
+            if (rpsbot == 'rock' && req.query.text == 'scissors') {
+                return res.send ({"text": "YOU LOSE! Bot rolled ROCK!"})
+            } else if (rpsbot == 'paper' && req.query.text == 'rock') {
+                return res.send ({"text": "YOU LOSE! Bot rolled PAPER!"})
+            } else if (rpsbot == 'scissors' && req.query.text == 'paper') {
+                return res.send ({"text": "YOU LOSE! Bot rolled SCISSORS!"})
+            } else if (rpsbot == 'rock' && req.query.text == 'paper') {
+                return res.send ({"text": "YOU WIN! Bot rolled ROCK!"})
+            } else if (rpsbot == 'paper' && req.query.text == 'scissors') {
+                return res.send ({"text": "YOU WIN! Bot rolled PAPER!"})
+            } else if (rpsbot == 'scissors' && req.query.text == 'rock') {
+                return res.send ({"text": "YOU WIN! Bot rolled SCISSORS!"})
+            } else if (rpsbot == req.query.text) {
+                return res.send ({"text": "TIE! You both rolled " + req.query.text + "!!!"})
+            }
+        }
+        else {
+            return res.send({"text": "You gotta do 'rock', 'paper', or 'scissors', duhh." + req.query.text});
+        }
+        
+        
+    });
+    app.get("/", function(req, res){
+        console.log("someone hit ye ole default");
+        return res.send("Hello Meow!");
+    });
+
+}
+
+var rps = [
+    'rock',
+    'paper',
+	'scissors'
+];
+
+var getrps = Math.floor(Math.random()*rps.length); 
+var rpsbot = ""; // define blank var
+function rockPaperScissorsBot () {
+    rpsbot = rps[getrps];
+}
+
+module.exports = appRouter;
