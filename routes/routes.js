1a3ac2ed13f9d203ef0669d59ceadeede5ddb9f3
cd83 <chrisdlee@gmail.com>
2016-07-09 15:19:55 -0500

    testing

--- routes/routes.js
+++ routes/routes.js
@@ -2,27 +2,10 @@ var appRouter = function(app){
     app.get("/status", function (req, res) {
         console.log("someone hit ye ole status");
         if(req.query.text){
-            // var rpsbot = rps[getrps];
-            // if (rpsbot == 'rock' && req.query.text == 'scissors') {
-            //     return res.send({"text": "YOU LOSE! Bot rolled ROCK!"});
-            // } else if (rpsbot == 'paper' && req.query.text == 'rock') {
-            //     return res.send({"text": "YOU LOSE! Bot rolled PAPER!"});
-            // } else if (rpsbot == 'scissors' && req.query.text == 'paper') {
-            //     return res.send({"text": "YOU LOSE! Bot rolled SCISSORS!"});
-            // } else if (rpsbot == 'rock' && req.query.text == 'paper') {
-            //     return res.send({"text": "YOU WIN! Bot rolled ROCK!"});
-            // } else if (rpsbot == 'paper' && req.query.text == 'scissors') {
-            //     return res.send({"text": "YOU WIN! Bot rolled PAPER!"});
-            // } else if (rpsbot == 'scissors' && req.query.text == 'rock') {
-            //     return res.send({"text": "YOU WIN! Bot rolled SCISSORS!"});
-            // } else if (rpsbot == req.query.text) {
-            //     return res.send({"text": "TIE! You both rolled " + req.query.text + "!!!"})
-            // }
+            return res.send({"text": "Boro says " + req.query.text});
         }
         else {
-            var rpsbot = rps[getrps];
-            return res.send({"text": rpsbot})
-            // return res.send({"text": "You gotta do 'rock', 'paper', or 'scissors', duhh."});
+            return res.send({"text": "Boro says howdy!"});
         }
         
         
@@ -34,14 +17,4 @@ var appRouter = function(app){
 
 }
 
-var rps = [
-    'rock',
-    'paper',
-	'scissors'
-];
-
-var getrps = Math.floor(Math.random()*rps.length);
-
-// function rockPaperScissorsBot () {
-//     rpsbot = rps[getrps];
-// }
+module.exports = appRouter;
